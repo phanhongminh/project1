1d88df0d0bec67a3e263e6d0deb3065279769f7e
Phan Hong Minh <phanhongminh95@gmail.com>
2016-06-20 11:19:41 +0700

    no message

---
+++ phanhongminh/test.py
@@ -0,0 +1,11 @@
+print('hello world')
+#nhap thu vien turtle
+from turtle import *
+shape("turtle") #doi hinh con rua
+
+for Y in range(3):
+ left(30)
+ for x in range(4):
+    forward(100)
+    left(90)
+
